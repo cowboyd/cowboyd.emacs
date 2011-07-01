fe7e13b3ba1774f456b3ebbbf81fbe704a773cfe
Charles Lowell <cowboyd@thefrontside.net>
2011-07-01 15:00:11 -0500

    * turn on menu bar * startup an emacs server for terminal clients to connect to * bind CMD-T to find-file-in project ala-textmate

--- cowboyd.el
+++ cowboyd.el
@@ -4,3 +4,16 @@
 (add-to-list 'load-path (concat user-specific-dir "/emacs-color-theme-solarized"))
 (require 'color-theme-solarized)
 (color-theme-solarized-light)
+
+;; why the hell isn't this on by default for OSX?
+(menu-bar-mode)
+
+;; let's start up a server so that we can fire up stuff on the command
+;; line
+(server-start)
+
+;; bind CMD-T to find file in project ala-textmate
+(global-set-key (kbd "s-t") 'find-file-in-project)
+
+
+
