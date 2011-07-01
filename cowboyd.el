0be72275a583783d11268c2a362cb0ec6aa35fb5
Charles Lowell <cowboyd@thefrontside.net>
2011-07-01 10:35:32 -0500

    load the solarized them by default.

---
+++ cowboyd.el
@@ -0,0 +1,6 @@
+
+;; I like the solarized theme
+
+(add-to-list 'load-path (concat user-specific-dir "/emacs-color-theme-solarized"))
+(require 'color-theme-solarized)
+(color-theme-solarized-light)
